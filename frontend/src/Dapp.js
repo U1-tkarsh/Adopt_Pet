530caf839d69e184ee2eff834144ba4a4943b6bf
Utkarsh Rai <raiutkarsh554@gmail.com>
2023-11-29 22:16:36 +0530

    smart contract, Metamask connected, frontend fixed

---
+++ frontend/src/Dapp.js
@@ -0,0 +1,84 @@
+import { useState, useEffect } from "react";
+import { Navbar } from "./components/Navbar";
+import { PetItem } from "./components/PetItem";
+import { TxError } from "./components/TxError";
+import { WalletNotDetected } from "./components/WalletNotDetected";
+import { ConnectWallet } from "./components/ConnectWallet";
+
+const HARDHAT_NETWORK_ID = Number(process.env.REACT_APP_NETWORK_ID);
+
+function Dapp() {
+
+  const [pets, setPets] = useState([]);
+  const [selectedAddress, setSelectedAddress] = useState(undefined);
+
+  useEffect(() => {
+    async function fetchPets() {
+      const res = await fetch("/pets.json");
+      const data = await res.json();
+      setPets(data);
+    } 
+
+    fetchPets();
+  }, []);
+
+  async function connectWallet() {
+    try {
+      const [address] = await window.ethereum.request({method: "eth_requestAccounts"});
+      await checkNetwork();
+      setSelectedAddress(address);
+      window.ethereum.on("accountsChanged", ([newAddress]) => {
+        if (newAddress === undefined) {
+          setSelectedAddress(undefined);
+          return;
+        }
+
+        setSelectedAddress(newAddress);
+        // connection to SC
+        // getting owned pets
+      });
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+
+  async function switchNetwork() {
+    const chainIdHex = `0x${HARDHAT_NETWORK_ID.toString(16)}`;
+    return await window.ethereum.request({
+      method: "wallet_switchEthereumChain",
+      params: [{chainId: chainIdHex}]
+    })
+  }
+
+  async function checkNetwork() {
+    if( window.ethereum.networkVersion !== HARDHAT_NETWORK_ID.toString() ){
+      return switchNetwork();
+    }
+
+    return null;
+  }
+
+  if (!window.ethereum ) {
+    return <WalletNotDetected />
+  }
+
+  if (!selectedAddress ) {
+    return <ConnectWallet connect={connectWallet}/>
+  }
+
+  return (
+    <div className="container">
+      <TxError />
+      <br />
+      <Navbar address={selectedAddress}/>
+
+      <div className="items">
+      { pets.map((pet) =>
+          <PetItem key={pet.id} pet={pet} />
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Dapp;
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,13 @@
+export function Navbar({ address }) {
+    return (
+      <div className="navbar-container">
+        <div className="navbar">
+          <div>Home</div>
+          <div>My Profile</div>
+        </div>
+        <div className="user-account">
+          Welcome {address}
+        </div>
+      </div>
+    )
+  }
