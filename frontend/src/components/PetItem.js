8708070f084384adeccb4205fe6cec79c27d4f48
Utkarsh Rai <raiutkarsh554@gmail.com>
2023-11-30 15:41:13 +0530

    final project

--- frontend/src/components/PetItem.js
+++ frontend/src/components/PetItem.js
@@ -1,37 +1,35 @@
-
-
-
-
-
-export function PetItem({pet}) {
-    return (
-      <div className="item">
-        <div className="image">
-          <img
-            src={pet.picture}
-            alt=""
-          ></img>
+export function PetItem({ pet, adoptPet, disabled, inProgress }) {
+  return (
+    <div className="item">
+      <div className="image">
+        <img src={pet.picture} alt=""></img>
+      </div>
+      <div className="info-holder">
+        <div>
+          <b>Name:</b> {pet.name}
         </div>
-        <div className="info-holder">
-          <div>
-            <b>Name:</b> {pet.name}
-          </div>
-          <div>
-            <b>Age:</b> {pet.age}
-          </div>
-          <div>
-            <b>Breed:</b> {pet.breed}
-          </div>
-          <div>
-            <b>Location:</b> {pet.location}
-          </div>
-          <div>
-            <b>Description:</b> {pet.description}
-          </div>
+        <div>
+          <b>Age:</b> {pet.age}
         </div>
-        <div className="action-menu">
-          <button className="action-button">Adopt</button>
+        <div>
+          <b>Breed:</b> {pet.breed}
+        </div>
+        <div>
+          <b>Location:</b> {pet.location}
+        </div>
+        <div>
+          <b>Description:</b> {pet.description}
         </div>
       </div>
-    )
-  }
+      <div className="action-menu">
+        
+          <button
+          className="action-button" 
+          onClick={adoptPet}
+          disabled={disabled || inProgress}>
+            { disabled ? "Happily Adopted" : "Adopt" }
+          </button>
+      </div>
+    </div>
+  );
+}
--- frontend/src/components/TxError.js
+++ frontend/src/components/TxError.js
@@ -1,11 +1,11 @@
-export function TxError() {
-    return (
-      <div className="message-warning" role="alert">
-        <div>Error sending transaction: Something went wrong...</div>
-        <br />
-        <button type="button dismiss-button" className="close">
-          <div>Dismiss</div>
-        </button>
-      </div>
+export function TxError({message, dismiss}) {
+  return (
+    <div className="message-warning" role="alert">
+      <div>Error sending transaction: {message}</div>
+      <br />
+      <button onClick={dismiss} type="button dismiss-button" className="close">
+        <div>Dismiss</div>
+      </button>
+    </div>
     )
   }
